6b0b93fc84848f3d8fc1a8e5a17bf8babb6f0cb9
nevermindReddds <vsetop1981@gmail.com>
2025-05-11 22:54:01 +0200

    Some commit inside submodule

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./App.module.scss";
 import { Details } from "./Details";
 
 export function App() {
   const [details, setDetails] = useState({
-    title: "null",
+    isLoading: false,
+    title: "useEffect",
     description: "Hello World!",
     buttonText: "Click",
   });
 
+  const imageRef = useRef(null);
+
+  const onClick = () => {
+    if (!imageRef.current) return;
+    imageRef.current.style.BorderRadius = "20px";
+    imageRef.current.style.BoxShadow = "0 3px 6px rgba(0,0,0, .1";
+    console.log(imageRef.current);
+  };
+
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(10);
+
+  const result = count * multiplier;
+
   return (
     <div className={styles.layout}>
-      <img src="/mid_348779_607834.jpg" width={300} />
+      <img ref={imageRef} src="/mid_348779_607834.jpg" width={300} />
 
-      <Details title={title} setTitle={setTitle} />
+      <div>Результат: {result}</div>
+
+      <button onClick={() => setCount(count + 1)}>Увеличить счетчик</button>
+      <button onClick={() => setMultiplier(multiplier + 10)}>
+        Увеличить множитель
+      </button>
+      <button onClick={onClick}>Change image</button>
+      {details.isLoading && <p>Loading...</p>}
+      <Details details={details} setDetails={setDetails} />
     </div>
   );
 }
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,9 +1,9 @@
-export function Details({ details, setTitle }) {
+export function Details({ details, handleLoading }) {
   return (
     <div>
       <h1>{details.title}</h1>
       <div>{details.description}</div>
-      <button onClick={() => se}>click</button>
+      <button onClick={handleLoading}>{details.ButtonText}</button>
     </div>
   );
 }
